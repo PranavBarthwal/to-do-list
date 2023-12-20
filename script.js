0d8b465af2b04822193a3a41346702d257ff5fad
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-12-20 05:03:04 +0000

    fix js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
 
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
-
-
-//Event handling, uder interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
